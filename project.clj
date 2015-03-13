90c0e854a3c4cafdd1a4b8ea6063301e2f8650a0
eprogramming <00hf11@gmail.com>
2015-03-13 00:38:51 -0300

    learning how to mix java and clj

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject dinga_invaders "0.1"
+  :description "hail from the other side!"
+  :license {:name "Apache License Version 2"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/main/java"]
+  :prep-tasks [["compile" "dinga_invaders.main"]
+               "javac" "compile"]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:uberjar {:aot :all}}
+  :main dinga_invaders.main)
+
---
+++ src/main/java/dinga_invaders/Hail.java
@@ -0,0 +1,10 @@
+package dinga_invaders;
+
+public class Hail {
+
+  @Override
+  public String toString() {
+    return "from the other side!";
+  }
+
+}
