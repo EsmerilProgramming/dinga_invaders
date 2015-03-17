7550d8610893a3d8bc1616f35b373a7511fd808f
eprogramming <00hf11@gmail.com>
2015-03-17 12:16:05 -0300

    bugado

--- src/clj/dinga_invaders/data.clj
+++ src/clj/dinga_invaders/data.clj
@@ -1,15 +1,15 @@
-(ns dinga_invaders.data
-  (:import java.awt.image.BufferedImage))
+(ns dinga_invaders.datai)
+(:import java.awt.image BufferedImage)
 
 ; tempo antes de uma interacao do loop do jogo para calular FPS
-(def startCycleProcess 0)
+(def start-cycle-process 0)
 ; tempo apos um ciclo terminar para calcular FPS
-(def endCycleProcess 1000)
+(def end-cycle-process 1000)
 ; incrementa coordenada Y de um retangulo usado para textura
 ; de background, ocorre uma cópia da img de background para TexturePaint
 ; para fazer efeito vertical scrolling é necessário incrementar o eixo Y
 ; do retangulo usado para construir esse o objeto textura ( no java )
-(def textureBackgroundY 0)
+(def texture-background-y 0)
 
 ; representa o espaço em background
 (let [space (BufferedImage.)])
