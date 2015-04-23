d83fba5704d4fdae6aff34649bda6098b4caf7be
eprogramming <00hf11@gmail.com>
2015-04-23 00:30:39 -0300

    sprite

--- dingainvaders/src/dingainvaders/core.clj
+++ dingainvaders/src/dingainvaders/core.clj
@@ -7,4 +7,6 @@
   [& args]
   (def frame (JFrame. "Dinga Invaders"))
   (.setSize frame 800 600)
+  (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
+  (.setLocationRelativeTo frame nil)
   (.setVisible frame true))
---
+++ dingainvaders/src/dingainvaders/sprite.clj
@@ -0,0 +1,8 @@
+(ns dingainvaders.sprite)
+
+(defn sprite
+  []
+  {:x 0
+   :y 0
+   :height 0
+   :width 0})
