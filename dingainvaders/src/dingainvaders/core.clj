8dd3c39c11d0ceffd307e94f1eacf1a613421960
eprogramming <00hf11@gmail.com>
2015-05-06 00:17:56 -0300

    still fkng hard...#13

--- dingainvaders/src/dingainvaders/core.clj
+++ dingainvaders/src/dingainvaders/core.clj
@@ -1,7 +1,24 @@
 (ns dingainvaders.core
   (:gen-class))
 
-(import 'javax.swing.JFrame)
+(import javax.swing.JFrame)
+(import java.awt.event.KeyListener)
+(import java.awt.image.BufferStrategy)
+(import java.awt.image.BufferedImage)
+(import java.awt.TexturePaint)
+(import java.awt.Rectangle)
+(import java.awt.Graphics2D)
+
+; [1] we need this:
+; private BufferStrategy bufferStrategy;
+; private BufferedImage bgImage;
+
+
+;(defn paint-bg) ; [3] we need need this:
+                ; Graphics2D g = (Graphics2D)bufferStrategy.getDrawGraphics();
+                ; bgImage = load an image using clojure here..
+                ; g.setPaint(new TexturePaint(bgImage, new Rectangle(0, 1, bgImage.getWidth(), bgImage.getHeight())));
+                ; g.fillRect(0, 0, getWidth(), getHeight()); not sure if we need this, btw width and height are from frame...
 
 (defn -main
   [& args]
@@ -11,4 +28,8 @@
     (.setLocationRelativeTo nil)
     (.setExtendedState JFrame/MAXIMIZED_BOTH)
     (.setUndecorated true)
-    (.setVisible true)))
+    (.setVisible true)
+    (.createBufferStrategy 2)
+    ; [2] then set the bufferStrategy with:
+    ; bufferStrategy = getBufferStrategy();
+    (.requestFocus)))
--- dingainvaders/src/dingainvaders/sprite.clj
+++ dingainvaders/src/dingainvaders/sprite.clj
@@ -4,5 +4,6 @@
   []
   {:x 0
    :y 0
+   :name
    :height 0
    :width 0})
